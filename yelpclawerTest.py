e7b9ae8d0c2642e5f8616b637654e874ad68bf0b
ab380765597 <zhangxi0728@gmail.com>
2016-06-03 09:17:13 -0700

    Update yelpclawerTest.py

--- yelpclawerTest.py
+++ yelpclawerTest.py
@@ -12,7 +12,7 @@ biz.printSchema()
 biz.registerTempTable("business")
 
 # SQL can be run over DataFrames that have been registered as a table.
-goodRes = sqlContext.sql("SELECT title, city, review_count FROM business WHERE rating >= 5 ORDER BY review_count DESC LIMIT 5")
+goodRes = sqlContext.sql("SELECT title, city, review_count FROM business WHERE rating >= 4.5 ORDER BY review_count DESC LIMIT 5")
 
 
 # The results of SQL queries are RDDs and support all the normal RDD operations.
